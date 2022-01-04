fde696c7384a636cf2b1c4082546d5c82686f838
natsufumij <natsufumij@163.com>
2022-01-04 18:10:41 +0800

    添加编辑器的程序

---
+++ Editor/src/main/java/albertgame/avg/editor/MainEntry.java
@@ -0,0 +1,16 @@
+package albertgame.avg.editor;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainEntry extends Application {
+    @Override
+    public void start(Stage primaryStage){
+
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
