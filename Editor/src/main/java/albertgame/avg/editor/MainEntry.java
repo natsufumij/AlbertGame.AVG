f08e9dd44d825aa3d46e6765509bad6800c51fb9
natsufumij <natsufumij@163.com>
2022-01-06 13:19:47 +0800

    初始化Editor

--- Editor/src/main/java/albertgame/avg/editor/MainEntry.java
+++ Editor/src/main/java/albertgame/avg/editor/MainEntry.java
@@ -2,28 +2,18 @@ package albertgame.avg.editor;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 public class MainEntry extends Application {
     @Override
     public void start(Stage primaryStage) {
 
         try {
-//            File f=new File("Assets/form.fxml");
-//            Parent parent = FXMLLoader.load(f.toURI().toURL());
-            Parent parent=FXMLLoader.load(MainEntry.class.getResource("a/form.fxml"));
-            URL url=MainEntry.class.getResource("a/1.avg");
-            System.out.println("url0:"+url.getPath());
-            url=MainEntry.class.getResource("config/images/bishojo_1.png");
-            System.out.println("url1:"+url.toExternalForm());
+            Parent parent=FXMLLoader.load(MainEntry.class.getResource("form.fxml"));
             Scene scene = new Scene(parent);
             primaryStage.setScene(scene);
             primaryStage.show();
