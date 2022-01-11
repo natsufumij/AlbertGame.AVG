949b39c93d281817d122b36416ea51e021653f72
natsufumij <natsufumij@163.com>
2022-01-12 00:45:09 +0800

    重构2.3

--- Editor/src/main/java/albertgame/avg/editor/ProgressHelp.java
+++ Editor/src/main/java/albertgame/avg/editor/ProgressHelp.java
@@ -1,5 +1,20 @@
 package albertgame.avg.editor;
 
+import javafx.scene.control.Dialog;
+import javafx.scene.control.ListCell;
+
 //帮助ProgressHelp
 public class ProgressHelp {
+
+    //Source_Id Option_id
+    //1 Expression1
+    //2 Expression2
+    //3 Expression3
+    //4 Expression4
+    public static class ProgressListCell extends ListCell<Progress>{
+        @Override
+        protected void updateItem(Progress item, boolean empty) {
+            super.updateItem(item, empty);
+        }
+    }
 }
